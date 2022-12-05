28e63167e0b79d48584baf3e0f3b9b36dea8ce1d
Ünal Parlak <unparlak@gmail.com>
2022-12-05 18:08:19 +0300

    type added to url, data is more specific

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -5,10 +5,12 @@ function useFetch(props: IProps) {
   const [data, setData] = useState();
 
   useEffect(() => {
-    fetch(`https://opentdb.com/api.php?amount=5&difficulty=${props.difficulty}`)
+    fetch(
+      `https://opentdb.com/api.php?amount=5&difficulty=${props.difficulty}&type=multiple`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
+        setData(data.results);
         //console.log(data);
       });
   }, [props.difficulty]);
