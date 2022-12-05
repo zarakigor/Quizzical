60f517c88607126b22ef36d297db1feaa4eb78ec
Ünal Parlak <unparlak@gmail.com>
2022-12-05 17:58:51 +0300

    custom tailwind colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#293264",
+      secondary: "#F5F7FB",
+      primary_bg: "#4D5B9E",
+    },
     extend: {
-      fontFamily: { karla: ["Karla", "Arial", "sans-serif"] },
-      inter: ["Inter", "Tahoma", "sans-serif"],
+      fontFamily: {
+        karla: ["Karla", "Arial", "sans-serif"],
+        inter: ["Inter", "Tahoma", "sans-serif"],
+      },
     },
   },
   plugins: [],
