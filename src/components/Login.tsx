220ac9d4ab7c2032261bdd414cd59823316bad32
Ünal Parlak <unparlak@gmail.com>
2022-12-05 17:59:12 +0300

    tailwind classes added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 function Login() {
   return (
-    <div className={""}>
-      <h1>Quizzical</h1>
-      <h3>Some description if needed</h3>
-      <button>Start Quiz</button>
+    <div className={"text-primary flex flex-col items-center"}>
+      <h1 className={"font-karla font-bold text-3xl"}>Quizzical</h1>
+      <h3 className={"font-inter mt-2 mb-7"}>Some description if needed</h3>
+      <button
+        className={
+          "text-secondary font-medium bg-primary_bg py-4 px-14 rounded-2xl"
+        }
+      >
+        Start Quiz
+      </button>
     </div>
   );
 }
