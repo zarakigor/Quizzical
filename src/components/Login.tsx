7fa61a4b910b37c1aa7dab4dd76208ebce1f271c
Ünal Parlak <unparlak@gmail.com>
2022-12-23 20:33:50 +0300

    buttons added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { useContext } from "react";
+import { Context } from "../Context";
 
 function Login() {
+  const AppContext = useContext(Context);
   return (
     <div className={"text-primary flex flex-col items-center"}>
       <h1 className={"font-karla font-bold text-3xl"}>Quizzical</h1>
       <h3 className={"font-inter mt-2 mb-7"}>Some description if needed</h3>
+
+      <div>
+        <button onClick={AppContext?.handleDifficulty}>Easy</button>
+        <button>Medium</button>
+        <button>Difficult</button>
+      </div>
+
       <button
         className={
           "text-secondary font-medium bg-primary_bg py-4 px-14 rounded-2xl"
