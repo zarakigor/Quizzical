21c73a473a50c50cb56cf6cf4ec06ac29d681953
Ünal Parlak <unparlak@gmail.com>
2022-12-23 20:35:38 +0300

    context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { Context } from "./Context";
+import Login from "./components/Login";
+import Questions from "./components/Questions";
 import useFetch from "./useFetch";
 
-import "./App.css";
-// https://opentdb.com/api.php?amount=5&difficulty=medium
-
 function App() {
-  const [difficulty, setDifficulty] = useState<string>("medium");
-  useFetch({ diff: difficulty });
-  return <div>asd</div>;
+  const AppContext = useContext(Context);
+
+  // burayı değiştir çalışmıyor
+  //const asd = useFetch({ difficulty: difficulty });
+
+  console.log(useContext(Context));
+
+  return (
+    <div className={"flex justify-center items-center h-screen"}>
+      {AppContext?.difficulty ? <Questions /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
+
+// https://opentdb.com/api.php?amount=5&difficulty=medium
