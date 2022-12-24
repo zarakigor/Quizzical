06dd00400aa7364e30c772cc4e44bd64ce478c7d
Ünal Parlak <unparlak@gmail.com>
2022-12-24 16:48:52 +0300

    type added to handleDifficulty function

--- src/Context.tsx
+++ src/Context.tsx
@@ -15,8 +15,10 @@ const Context = createContext<IContext | null>(null);
 function ContextProvider({ children }: IProps) {
   const [difficulty, setDifficulty] = useState(null);
 
-  function handleDifficulty() {
-    console.log("button çalışıyor");
+  function handleDifficulty(event: React.ChangeEvent<HTMLButtonElement>) {
+    //console.log("button çalışıyor");
+    //const target = event.target as HTMLInputElement;
+    console.log(event.target.value);
   }
 
   return (
