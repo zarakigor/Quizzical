b24f3cb2a3b96e3fd8fcac579b6b93748413bd1e
Ünal Parlak <unparlak@gmail.com>
2023-01-05 22:41:57 +0300

    fetch data added

--- src/Context.tsx
+++ src/Context.tsx
@@ -19,6 +19,15 @@ function ContextProvider({ children }: IContextProvider) {
   const [difficulty, setDifficulty] = useState<string | null>(null);
   const [isQuestionsReady, setIsQuestionsReady] = useState<boolean>(false);
 
+  const [data, setData] = useState<any[]>([]);
+  const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
+
+  // const [data, setData] = useState({
+  //   question: "",
+  //   answers: [],
+  //   correct_answer: "",
+  // });
+
   function handleDifficulty(event: React.MouseEvent<HTMLButtonElement>): void {
     console.log(event.currentTarget.value);
     setDifficulty(event.currentTarget.value);
@@ -27,8 +36,31 @@ function ContextProvider({ children }: IContextProvider) {
   function startQuiz() {
     if (difficulty) {
       setIsQuestionsReady(true);
+      filterCorrectAnswers();
+      //console.log(data[0]);
     }
   }
+  useEffect(() => {
+    fetch(
+      `https://opentdb.com/api.php?amount=5&difficulty=${difficulty}&type=multiple`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        //setData(data.results);
+
+        // console.log(data.results[0].correct_answer);
+        // console.log(data.results[0]?.incorrect_answers);
+        // for (let i = 0; i < 5; i++) {
+        //   console.log(data.results[i]);
+        // }
+        setData(data.results);
+      });
+  }, [difficulty]);
+
+  function filterCorrectAnswers() {
+    setCorrectAnswers(data.map((x) => x.correct_answer));
+  }
+  console.log(correctAnswers);
 
   return (
     <Context.Provider
