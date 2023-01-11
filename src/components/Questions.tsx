520c296df3e892a50519bed01b053f91b62b2e8f
Ünal Parlak <unparlak@gmail.com>
2023-01-11 22:25:26 +0300

    basic styling added for questions and options

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -10,21 +10,34 @@ function Questions() {
     return [...arr].sort(() => Math.random() - 0.5);
   }
   return (
-    <>
+    <div className={"mt-10 mx-10"}>
       {AppContext?.data.map((element: IData) => {
         return (
-          <div key={nanoid()}>
-            <p> {element.question}</p>
+          <div key={nanoid()} className={"text-primary"}>
+            <p className={"font-karla font-bold "}>{element.question}</p>
             <div>
               {shuffle(element.options!).map((option) => {
-                return <p key={nanoid()}>{option}</p>;
+                return (
+                  <button
+                    key={nanoid()}
+                    className={
+                      "font-inter font-medium text-xs border border-primary px-2 py-1 rounded-lg mt-3 mr-3"
+                    }
+                    value={option}
+                  >
+                    {option}
+                  </button>
+                );
               })}
             </div>
+            <hr className={"text-secondary_bg my-4"}></hr>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
 
 export default Questions;
+
+// mr3 button daki , onu flexe gride bağlamaya çalış
