16aca7c1ae6abc7c24f00cc94800104e6bfaec4e
Ünal Parlak <unparlak@gmail.com>
2023-01-14 22:06:00 +0300

    move colors under extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#293264",
-      secondary: "#F5F7FB",
-      primary_bg: "#4D5B9E",
-      secondary_bg: "#D6DBF5",
-    },
     extend: {
+      colors: {
+        primary: "#293264",
+        secondary: "#F5F7FB",
+        primary_bg: "#4D5B9E",
+        secondary_bg: "#D6DBF5",
+      },
       fontFamily: {
         karla: ["Karla", "Arial", "sans-serif"],
         inter: ["Inter", "Tahoma", "sans-serif"],
