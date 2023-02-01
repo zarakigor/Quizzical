c27f7fb86126f871442098e659628c4d1efb43c4
Ünal Parlak <unparlak@gmail.com>
2023-02-01 22:59:54 +0300

    new colors for wrong and correct answers added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         secondary: "#F5F7FB",
         primary_bg: "#4D5B9E",
         secondary_bg: "#D6DBF5",
+        wrong_answer_bg: "#F8BCBC",
+        correct_answer_bg: "#94D7A2",
       },
       fontFamily: {
         karla: ["Karla", "Arial", "sans-serif"],
