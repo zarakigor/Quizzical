e4f61a0a0f5916a04dcdf853ee0c83427949b559
Ünal Parlak <unparlak@gmail.com>
2023-02-16 13:38:09 +0300

    function names renamed, shuffle function removed

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -5,43 +5,33 @@ import { nanoid } from "nanoid";
 function Quiz(props: IData) {
   const AppContext = useContext(Context);
 
-  // to prevent the choices from being displayed in alphabetical order
-  function shuffle(arr: string[]) {
-    return [...arr].sort(() => Math.random() - 0.5);
-  }
-
-  function handleChosenChoices(choice: string) {
+  function updateChosenChoices(choice: string) {
     let arr: string[] | undefined = AppContext?.chosenChoices;
     arr![props.index] = choice;
     AppContext?.setChosenChoices(arr!);
   }
 
-  function handleCheckAnswers(e: any) {
-    //console.log(e.target.value);
-    //console.log(props.index);
-    console.log(props.correct_answer);
-
+  function handleClickChoices(e: any) {
     for (let i = 0; i < 4; i++) {
       let element = document.getElementsByClassName(`${props.index}`)[i];
       element.classList.remove("bg-secondary_bg", "border-transparent");
     }
 
-    handleChosenChoices(e.target.value);
+    updateChosenChoices(e.target.value);
     e.target.classList.add("bg-secondary_bg", "border-transparent");
-    console.log(AppContext?.chosenChoices);
   }
 
   return (
     <div>
       <p className={"text-primary font-karla font-bold "}>{props.question}</p>
       <div>
-        {shuffle(props.choices).map((choice) => {
+        {props.choices.map((choice) => {
           return (
             <button
               key={nanoid()}
               id={choice}
-              className={`${props.index} text-primary font-inter font-medium text-xs border border-primary  px-2 py-1 rounded-lg mt-3 mr-3`}
-              onClick={(e) => handleCheckAnswers(e)}
+              className={`${props.index} choice text-primary font-inter font-medium text-xs border border-primary  px-2 py-1 rounded-lg mt-3 mr-3`}
+              onClick={(e) => handleClickChoices(e)}
               value={choice}
             >
               {choice}
